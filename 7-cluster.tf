f6f2f4e7af7720adf582d5733ff8e2b0f236bd31
CloudNinja92 <144071013+CloudNinja92@users.noreply.github.com>
2024-10-06 15:12:52 -0700

    Update 7-cluster.tf

--- 7-cluster.tf
+++ 7-cluster.tf
@@ -1,46 +1,23 @@
-resource "google_container_cluster" "primary" {
-  name                     = "primary"
-  location                 = "us-central1-a"
-  remove_default_node_pool = true
-  initial_node_count       = 1
-  network                  = google_compute_network.cluster-vpc.self_link
-  subnetwork               = google_compute_subnetwork.private.self_link
-  logging_service          = "logging.googleapis.com/kubernetes"
-  monitoring_service       = "monitoring.googleapis.com/kubernetes"
-  networking_mode          = "VPC_NATIVE"
+resource "google_compute_firewall" "allow-ssh" {
+  name    = "allow-ssh"
+  network = google_compute_network.cluster-vpc.name
 
-  # Optional, if you want multi-zonal cluster
-  node_locations = [
-    "us-central1-b"
-  ]
-
-  addons_config {
-    http_load_balancing {
-      disabled = true
-    }
-    horizontal_pod_autoscaling {
-      disabled = false
-    }
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
   }
 
-  release_channel {
-    channel = "REGULAR"
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_firewall" "allow-http" {
+  name    = "allow-http"
+  network = google_compute_network.cluster-vpc.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
   }
 
-  workload_identity_config {
-    workload_pool = "${var.project}.svc.id.goog"
-  }
-
-  ip_allocation_policy {
-    cluster_secondary_range_name  = "k8s-pod-range"
-    services_secondary_range_name = "k8s-service-range"
-  }
-
-  private_cluster_config {
-    enable_private_nodes    = true
-    enable_private_endpoint = false
-    master_ipv4_cidr_block  = "172.16.0.0/28"
-  }
-
-  deletion_protection = false
+  source_ranges = ["0.0.0.0/0"]
 }
